e3532d0829b8a4074de9d807a2a4cbaf29e7c12d
Kaiming-Wei <kwei002@citymail.cuny.edu>
2024-10-29 01:02:50 -0400

    Workshop 3

--- src/camera_utils.py
+++ src/camera_utils.py
@@ -57,7 +57,15 @@ def compute_image_footprint_on_surface(camera: Camera, distance_from_surface: fl
     Returns:
         np.ndarray: [footprint_x, footprint_y] in meters.
     """
-    raise NotImplementedError()
+    fx = camera.fx  # focal width
+    fy = camera.fy  # focal length
+    image_width = camera.image_size_x_px
+    image_length = camera.image_size_y_px 
+
+    footprint_width = (distance_from_surface / fx) * image_width
+    footprint_length = (distance_from_surface / fy) * image_length
+
+    return np.array([footprint_width, footprint_length], dtype=np.float32)
 
 def compute_ground_sampling_distance(camera: Camera, distance_from_surface: float) -> float:
     """Compute the ground sampling distance (GSD) at a given distance from the surface.
@@ -69,4 +77,33 @@ def compute_ground_sampling_distance(camera: Camera, distance_from_surface: floa
     Returns:
         float: the GSD in meters (smaller among x and y directions).
     """
-    raise NotImplementedError()
+    # GSD = footprint / number of pixels
+    footprint_x,footprint_y = compute_image_footprint_on_surface(camera, distance_from_surface)
+    GSD_x = footprint_x / camera.image_size_x_px
+    GSD_y = footprint_y / camera.image_size_y_px
+
+    return GSD_x
+
+
+def reproject_image_point_to_world(camera: Camera, pixel_point: np.ndarray, depth: float) -> np.ndarray:
+    """
+    Reproject a 2D pixel location back to a 3D world point given the camera model and depth.
+
+    Args:
+        camera (Camera): the camera model.
+        pixel_point (np.ndarray): the (x, y) coordinates in the image plane.
+        depth (float): the depth (distance from the camera to the point) in meters.
+
+    Returns:
+        np.ndarray: the corresponding 3D world coordinates [X, Y, Z].
+    """
+    # Extract pixel coordinates
+    u, v = pixel_point
+
+    # Reproject to normalized camera coordinates
+    x = (u - camera.cx) * depth / camera.fx
+    y = (v - camera.cy) * depth / camera.fy
+    z = depth  # The depth corresponds to the Z-coordinate in world space
+
+    # Return the 3D coordinates
+    return np.array([x, y, z], dtype=int)
--- src/data_model.py
+++ src/data_model.py
@@ -10,14 +10,25 @@ class Camera:
     - https://github.com/colmap/colmap/blob/3f75f71310fdec803ab06be84a16cee5032d8e0d/src/colmap/sensor/models.h#L220
     - https://en.wikipedia.org/wiki/Pinhole_camera_model
     """
+
+    # We want to model the following camera parameters in Python:
+    # - focal length along x axis (in pixels)
+    # - focal length along y axis (in pixels)
+    # - optical center of the image along the x axis (in pixels)
+    # - optical center of the image along the y axis (in pixels)
+    # - Size of the sensor along the x axis (in mm)
+    # - Size of the sensor along the y axis (in mm)
+    # - Number of pixels in the image along the x axis
+    # - Number of pixels in the image along the y axis
+
     fx : float
     fy : float
     cx : float
     cy : float
     sensor_size_x_mm : float
     sensor_size_y_mm : float
-    image_size_x : int
-    image_size_y : int
+    image_size_x_px : int
+    image_size_y_px : int
 
 
 @dataclass
@@ -25,7 +36,19 @@ class DatasetSpec:
     """
     Data model for specifications of an image dataset.
     """
-    pass
+
+    # - Overlap: the ratio (in 0 to 1) of scene shared between two consecutive images.
+    # - Sidelap: the ratio (in 0 to 1) of scene shared between two images in adjacent rows.
+    # - Height: the height of the scan above the ground (in meters).
+    # - Scan_dimension_x: the horizontal size of the rectangle to be scanned
+    # - Scan_dimension_y: the vertical size of the rectangle to be scanned
+    # - exposure_time_ms: the exposure time for each image (in milliseconds).
+    overlap : float
+    sidelap : float
+    height : float
+    scan_dimension_x : float
+    scan_dimension_y : float
+    exposure_time_ms : float
 
 
 @dataclass
